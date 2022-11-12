5f5bd853f54dddadbcc3290b04d929fee0141d0d
AhmadSaadat <saadatahmad267@gmail.com>
2022-11-12 15:44:02 +0330

    Add,Edit,Home,View Component Complete

--- MehrSepand/src/app/app.component.ts
+++ MehrSepand/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'MehrSepand';
+
+  constructor(private router: Router) { }
+
+  HomeClick(){
+    this.router.navigate(['Home']);
+  }
 }
